9a631c36ee5d22118666471bc2751a0a392b11cb
Casey Morgan <caseyjeanmorgan@gmail.com>
2020-10-13 16:03:24 -0500

    code for index.js

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Readme-Generator
+A README generator that quickly creates a professional README for a new project.
+
+## Description
+A README generator for any application to be created. It includes what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions. 
+
+## Links
+- Deployed URL &mdash; https://cc22389.github.io/Readme-Generator/
+- Repository &mdash; https://github.com/cc22389/Readme-Generator.git
+
+## Video Walkthrough
+- 
+
+## Screenshots
+- Sample README &mdash; 
+- Terminal Code &mdash; 
+
+
+## Credits
+- Casey Morgan
---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "project name",
+        message: "What is your project's name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: [
+            "MIT",
+            "Other",
+            "GP:v2",
+            "Apache",
+            "GPLv3",
+            "BSD 3-clause",
+            "Unlicense",
+            "BSD 2-clause",
+            "LGPLv3",
+            "AGP:v3"
+        ]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "what does the user need to know about contributing to the repository?"
+    }
+];
+
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), filename), data);
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerResponses) => {
+        console.log("Generating README...");
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+    })
+}
+
+init();
